d91a64ddfa3ca017e47e3497f3c54f720cc9ecaf
Anurag <Anurag@LAPTOP-ANURAG>
2022-12-07 15:54:36 +0530

    code changes

---
+++ CLI_Products/ProductsDataLayer/DTO/ProductsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI_Products.SaasProducts.DTO
+{
+    public class ProductsDto
+    {
+        public string Categories { get; set; }
+        public string? Twitter { get; set; }
+        public string Name { get; set; }
+    }
+}
